29ace9e43e7598af38953f91ebc1f75845480eb7
tyrozty <tianyuzhang@Tianyus-MacBook-Pro.local>
2018-12-16 19:53:02 -0500

    work version for CRUD

--- YelpData/YelpData/urls.py
+++ YelpData/YelpData/urls.py
@@ -4,17 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutPageView.as_view(), name='about'),
-    path('users/', views.UserListView.as_view(), name='users'),
+    path('users/', views.UserListView.as_view(), name='user'),
     path('users/<int:pk>/', views.UserDetailView.as_view(), name='user_detail'),
-    path('businesses/', views.BusinessListView.as_view(), name='businesses'),
+    path('businesses/', views.BusinessListView.as_view(), name='business'),
     path('businesses/<int:pk>/', views.BusinessDetailView.as_view(), name='business_detail'),
-    path('reviews/', views.ReviewListView.as_view(), name='reviews'),
+    path('reviews/', views.ReviewListView.as_view(), name='review'),
     path('reviews/<int:pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
-    path('tips/', views.TipListView.as_view(), name='tips'),
+    path('tips/', views.TipListView.as_view(), name='tip'),
     path('tips/<int:pk>/', views.TipDetailView.as_view(), name='tip_detail'),
-    path('photos/', views.PhotoListView.as_view(), name='photos'),
+    path('photos/', views.PhotoListView.as_view(), name='photo'),
     path('photos/<int:pk>/', views.PhotoDetailView.as_view(), name='photo_detail'),
-    path('reviews/new/', views.UserCreateView.as_view(), name='user_new'),
-    path('reviews/<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
-    path('reviews/<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete')
+    path('users/new/', views.UserCreateView.as_view(), name='user_new'),
+    path('users/<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
+    path('users/<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete')
 ]
