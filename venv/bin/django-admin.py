a18dd948a01bbfd0bc58abd869640bc16044d4d6
tyrozty <tyrozty@gmail.com>
2018-12-22 14:40:45 -0500

    Removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/tianyuzhang/Desktop/SI664/YelpDataBaseSQLAPP/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
