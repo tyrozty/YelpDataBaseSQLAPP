9c1195f002718b3eabb241abf5cdb897d149c64a
tyrozty <tyrozty@gmail.com>
2018-12-22 09:14:21 -0500

    adding venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tianyuzhang/Desktop/SI664/YelpDataBaseSQLAPP/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
